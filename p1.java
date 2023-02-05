b6a0d11fd5b42df55f949d09f8f12556ff86f13e
anuragAP1 <anuragparimal@gmail.com>
2023-02-05 15:56:46 +0530

    first commit

---
+++ p1.java
@@ -0,0 +1,12 @@
+public class prog1
+{
+    //static int b=10;
+    public static void main(String[] args)
+    {
+        int b;
+        b=1;
+        System.out.println(b);
+        //System.out.println(prog1.b);
+
+    }
+}
---
+++ p2.java
@@ -0,0 +1,15 @@
+public class prog1
+{
+    //static int b=10;
+    public static void main(String[] args)
+    {
+        int b;
+        b=1;
+        System.out.println(b);
+        //System.out.println(prog1.b);
+	int b;
+        b=1;
+        System.out.println(b);
+
+    }
+}
